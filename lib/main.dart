554b1c28d578982bb2d116433af11e1c54689dc3
linmyatoo <linmyatooa7@gmail.com>
2025-11-07 19:44:33 +0700

    implement forgot password

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
+
 import 'src/app_shell.dart';
-import 'src/features/splash/admin_splash_page.dart';
-import 'src/features/onboarding/onboarding_page.dart';
+import 'src/core/theme/app_colors.dart';
+import 'src/features/auth/forgot_password_page.dart';
 import 'src/features/auth/login_page.dart';
 import 'src/features/auth/sign_up_page.dart';
+import 'src/features/create/create_event_page.dart';
 import 'src/features/home/home_page.dart';
+import 'src/features/notifications/notification_feed_page.dart';
+import 'src/features/onboarding/onboarding_page.dart';
+import 'src/features/profile/change_password_page.dart';
+import 'src/features/profile/help_center_page.dart';
+import 'src/features/profile/notifications_settings_page.dart';
+import 'src/features/profile/personal_data_page.dart';
+import 'src/features/profile/privacy_policy_page.dart';
+import 'src/features/profile/profile_page.dart';
+import 'src/features/profile/security_page.dart';
 import 'src/features/reviews/reviews_page.dart';
 import 'src/features/sales/sales_page.dart';
-import 'src/features/profile/profile_page.dart';
-import 'src/core/theme/app_colors.dart';
-
-import 'src/features/create/create_event_page.dart';
-import 'src/features/profile/personal_data_page.dart';
-import 'src/features/profile/security_page.dart';
-import 'src/features/profile/notifications_settings_page.dart';
-import 'src/features/profile/privacy_policy_page.dart';
-import 'src/features/profile/help_center_page.dart';
-import 'src/features/profile/change_password_page.dart';
-import 'src/features/notifications/notification_feed_page.dart';
+import 'src/features/splash/admin_splash_page.dart';
 
 void main() {
   runApp(const TripNestApp());
@@ -77,7 +78,8 @@ class TripNestApp extends StatelessWidget {
         ),
 
         dialogTheme: DialogThemeData(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
           backgroundColor: Colors.white,
           surfaceTintColor: Colors.transparent,
         ),
@@ -88,16 +90,17 @@ class TripNestApp extends StatelessWidget {
         OnboardingPage.route: (_) => const OnboardingPage(),
         LoginPage.route: (_) => const LoginPage(),
         SignUpPage.route: (_) => const SignUpPage(),
+        ForgotPasswordPage.route: (_) => const ForgotPasswordPage(),
         AppShell.route: (_) => const AppShell(),
         HomePage.route: (_) => const HomePage(),
         ReviewsPage.route: (_) => const ReviewsPage(),
         SalesPage.route: (_) => const SalesPage(),
         ProfilePage.route: (_) => const ProfilePage(),
-
         CreateEventPage.route: (_) => const CreateEventPage(),
         PersonalDataPage.route: (_) => const PersonalDataPage(),
         SecurityPage.route: (_) => const SecurityPage(),
-        NotificationsSettingsPage.route: (_) => const NotificationsSettingsPage(),
+        NotificationsSettingsPage.route: (_) =>
+            const NotificationsSettingsPage(),
         PrivacyPolicyPage.route: (_) => const PrivacyPolicyPage(),
         HelpCenterPage.route: (_) => const HelpCenterPage(),
         ChangePasswordPage.route: (_) => const ChangePasswordPage(),
